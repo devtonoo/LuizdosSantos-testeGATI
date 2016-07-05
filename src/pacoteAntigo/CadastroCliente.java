70469604b898636c1213354d5adcedf6a0ac53e2
Luiz dos Santos <luizdeverdade@gmail.com>
2016-07-05 15:08:02 -0300

    Commit com adicao de propriedade de configuracao das variaveis do cliente

--- src/pacoteAntigo/CadastroCliente.java
+++ src/pacoteAntigo/CadastroCliente.java
@@ -10,7 +10,7 @@ package pacoteAntigo;
  * @author homeoffice
  */
 public class CadastroCliente extends javax.swing.JFrame {
-
+    
     /**
      * Creates new form CadastroCliente
      */
@@ -30,14 +30,14 @@ public class CadastroCliente extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
+        labelNome = new javax.swing.JLabel();
+        labelEmal = new javax.swing.JLabel();
+        labelTelefone = new javax.swing.JLabel();
+        labelEndereco = new javax.swing.JLabel();
+        labelNumero = new javax.swing.JLabel();
+        labelBairro = new javax.swing.JLabel();
+        labelCEP = new javax.swing.JLabel();
+        labelStatus = new javax.swing.JLabel();
         nome = new javax.swing.JTextField();
         email = new javax.swing.JTextField();
         telefone = new javax.swing.JTextField();
@@ -46,6 +46,7 @@ public class CadastroCliente extends javax.swing.JFrame {
         bairro = new javax.swing.JTextField();
         cep = new javax.swing.JTextField();
         ativo = new javax.swing.JCheckBox();
+        jLabel9 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Cadastro Clientes GATI");
@@ -72,21 +73,21 @@ public class CadastroCliente extends javax.swing.JFrame {
             }
         });
 
-        jLabel1.setText("Nome: ");
+        labelNome.setText("Nome: ");
 
-        jLabel2.setText("Email");
+        labelEmal.setText("Email");
 
-        jLabel3.setText("Telefone: ");
+        labelTelefone.setText("Telefone: ");
 
-        jLabel4.setText("Endereco: ");
+        labelEndereco.setText("Endereco: ");
 
-        jLabel5.setText("Numero: ");
+        labelNumero.setText("Numero: ");
 
-        jLabel6.setText("Bairro:");
+        labelBairro.setText("Bairro:");
 
-        jLabel7.setText("CEP: ");
+        labelCEP.setText("CEP: ");
 
-        jLabel8.setText("Status do cliente: ");
+        labelStatus.setText("Status do cliente: ");
 
         numero.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -96,6 +97,9 @@ public class CadastroCliente extends javax.swing.JFrame {
 
         ativo.setText("Ativo");
 
+        jLabel9.setFont(new java.awt.Font("Ubuntu", 1, 18)); // NOI18N
+        jLabel9.setText("Cadastro de cliente - GATI");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -103,84 +107,93 @@ public class CadastroCliente extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(42, 42, 42)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 202, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 205, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap())
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel2)
-                                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING))
+                                    .addComponent(labelEmal)
+                                    .addComponent(labelTelefone, javax.swing.GroupLayout.Alignment.TRAILING))
                                 .addGap(31, 31, 31))
                             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addComponent(jLabel1)
+                                .addComponent(labelNome)
                                 .addGap(44, 44, 44)))
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(email)
-                            .addComponent(telefone)
-                            .addComponent(endereco)
                             .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                    .addComponent(numero, javax.swing.GroupLayout.DEFAULT_SIZE, 66, Short.MAX_VALUE)
-                                    .addComponent(bairro))
-                                .addGap(0, 0, Short.MAX_VALUE))
-                            .addComponent(nome))
-                        .addGap(177, 177, 177))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(email)
+                                    .addComponent(telefone)
+                                    .addComponent(endereco)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                            .addComponent(numero, javax.swing.GroupLayout.DEFAULT_SIZE, 66, Short.MAX_VALUE)
+                                            .addComponent(bairro))
+                                        .addGap(0, 0, Short.MAX_VALUE))
+                                    .addComponent(nome))
+                                .addGap(177, 177, 177))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel9, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addContainerGap())))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel6)
-                            .addComponent(jLabel5)
-                            .addComponent(jLabel4)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                    .addComponent(jLabel7)
-                                    .addGap(18, 18, 18)
-                                    .addComponent(cep))
-                                .addGroup(layout.createSequentialGroup()
-                                    .addComponent(jLabel8)
-                                    .addGap(18, 18, 18)
-                                    .addComponent(ativo))))
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 202, Short.MAX_VALUE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 205, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(labelBairro)
+                                    .addComponent(labelNumero)
+                                    .addComponent(labelEndereco)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                            .addComponent(labelCEP)
+                                            .addGap(18, 18, 18)
+                                            .addComponent(cep))
+                                        .addGroup(layout.createSequentialGroup()
+                                            .addComponent(labelStatus)
+                                            .addGap(18, 18, 18)
+                                            .addComponent(ativo))))
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(44, 44, 44)
+                .addContainerGap()
+                .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
+                    .addComponent(labelNome)
                     .addComponent(nome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel2)
+                    .addComponent(labelEmal)
                     .addComponent(email, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
+                    .addComponent(labelTelefone)
                     .addComponent(telefone, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
+                    .addComponent(labelEndereco)
                     .addComponent(endereco, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel5)
+                    .addComponent(labelNumero)
                     .addComponent(numero, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel6)
+                    .addComponent(labelBairro)
                     .addComponent(bairro, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel7)
+                    .addComponent(labelCEP)
                     .addComponent(cep, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel8)
+                    .addComponent(labelStatus)
                     .addComponent(ativo))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -210,38 +223,7 @@ public class CadastroCliente extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_numeroActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CadastroCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CadastroCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CadastroCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CadastroCliente.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> {
-            new CadastroCliente().setVisible(true);
-        });
-    }
+    
   
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -253,14 +235,15 @@ public class CadastroCliente extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JLabel labelBairro;
+    private javax.swing.JLabel labelCEP;
+    private javax.swing.JLabel labelEmal;
+    private javax.swing.JLabel labelEndereco;
+    private javax.swing.JLabel labelNome;
+    private javax.swing.JLabel labelNumero;
+    private javax.swing.JLabel labelStatus;
+    private javax.swing.JLabel labelTelefone;
     private javax.swing.JTextField nome;
     private javax.swing.JTextField numero;
     private javax.swing.JTextField telefone;
--- src/pacoteAntigo/Cliente.java
+++ src/pacoteAntigo/Cliente.java
@@ -1,18 +1,129 @@
 package pacoteAntigo;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 /**
  *
- * @author homeoffice
+ * @author Luiz Fernando dos Santos em homeoffice
  */
 public class Cliente {
     
     private String nome;
+    private String nomeEntreda;
+
+    public static final String PROP_NOMEENTREDA = "nomeEntreda";
+
+    public String getNomeEntreda() {
+        return nomeEntreda;
+    }
+
+    public void setNomeEntreda(String nomeEntreda) {
+        String oldNomeEntreda = this.nomeEntreda;
+        this.nomeEntreda = nomeEntreda;
+        propertyChangeSupport.firePropertyChange(PROP_NOMEENTREDA, oldNomeEntreda, nomeEntreda);
+    }
+
+    private transient final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        propertyChangeSupport.removePropertyChangeListener(listener);
+    }
+
     private String email;
+    private String emailEntrada;
+
+    public static final String PROP_EMAILENTRADA = "emailEntrada";
+
+    public String getEmailEntrada() {
+        return emailEntrada;
+    }
+
+    public void setEmailEntrada(String emailEntrada) {
+        String oldEmailEntrada = this.emailEntrada;
+        this.emailEntrada = emailEntrada;
+        propertyChangeSupport.firePropertyChange(PROP_EMAILENTRADA, oldEmailEntrada, emailEntrada);
+    }
+
     private String telefone;
+    private String telefoneEntreda;
+
+    public static final String PROP_TELEFONEENTREDA = "telefoneEntreda";
+
+    public String getTelefoneEntreda() {
+        return telefoneEntreda;
+    }
+
+    public void setTelefoneEntreda(String telefoneEntreda) {
+        String oldTelefoneEntreda = this.telefoneEntreda;
+        this.telefoneEntreda = telefoneEntreda;
+        propertyChangeSupport.firePropertyChange(PROP_TELEFONEENTREDA, oldTelefoneEntreda, telefoneEntreda);
+    }
+
     private String endereco;
+    private String enderecoEntreda;
+
+    public static final String PROP_ENDERECOENTREDA = "enderecoEntreda";
+
+    public String getEnderecoEntreda() {
+        return enderecoEntreda;
+    }
+
+    public void setEnderecoEntreda(String enderecoEntreda) {
+        String oldEnderecoEntreda = this.enderecoEntreda;
+        this.enderecoEntreda = enderecoEntreda;
+        propertyChangeSupport.firePropertyChange(PROP_ENDERECOENTREDA, oldEnderecoEntreda, enderecoEntreda);
+    }
+
     private int numero;
+    private int numeroEntrada;
+
+    public static final String PROP_NUMEROENTRADA = "numeroEntrada";
+
+    public int getNumeroEntrada() {
+        return numeroEntrada;
+    }
+
+    public void setNumeroEntrada(int numeroEntrada) {
+        int oldNumeroEntrada = this.numeroEntrada;
+        this.numeroEntrada = numeroEntrada;
+        propertyChangeSupport.firePropertyChange(PROP_NUMEROENTRADA, oldNumeroEntrada, numeroEntrada);
+    }
+
     private String bairro;
+    private String bairroEntrada;
+
+    public static final String PROP_BAIRROENTRADA = "bairroEntrada";
+
+    public String getBairroEntrada() {
+        return bairroEntrada;
+    }
+
+    public void setBairroEntrada(String bairroEntrada) {
+        String oldBairroEntrada = this.bairroEntrada;
+        this.bairroEntrada = bairroEntrada;
+        propertyChangeSupport.firePropertyChange(PROP_BAIRROENTRADA, oldBairroEntrada, bairroEntrada);
+    }
+
     private int cep;
+    private int cepEntrada;
+
+    public static final String PROP_CEPENTRADA = "cepEntrada";
+
+    public int getCepEntrada() {
+        return cepEntrada;
+    }
+
+    public void setCepEntrada(int cepEntrada) {
+        int oldCepEntrada = this.cepEntrada;
+        this.cepEntrada = cepEntrada;
+        propertyChangeSupport.firePropertyChange(PROP_CEPENTRADA, oldCepEntrada, cepEntrada);
+    }
+
     private boolean ativo;
    
     public Cliente(String nome, String email, String telefone, String endereco,
